6fc4258856127ee96c5665aa46dbeca7d855f788
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2016-12-04 12:28:17 -0800

    Added some assets laid out a scroll view and tested

--- PageScrolling/ViewController.swift
+++ PageScrolling/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            images.append(UIImageView(image: image))
+            
+        }
+        
+        print("Count: \(images.count)")
     }
 
     override func didReceiveMemoryWarning() {
